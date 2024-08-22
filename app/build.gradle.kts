fc221c2fddc73d6a44f6a0d21295101403a43c10
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2024-08-22 10:11:18 +0200

    Enable compose reports in kotlin 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    composeCompiler {
+        reportsDestination = file("build/outputs/compose-reports")
+        metricsDestination = file("build/outputs/compose-metrics")
+    }
 }
 
 dependencies {
